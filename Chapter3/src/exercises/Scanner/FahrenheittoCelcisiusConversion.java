d1cb1b1a8145311ae930797d27436286a10466ab
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-12 09:25:52 -0500

    FahrenheittoCelcius

--- Chapter3/src/exercises/Scanner/FahrenheittoCelcisiusConversion.java
+++ Chapter3/src/exercises/Scanner/FahrenheittoCelcisiusConversion.java
@@ -10,9 +10,20 @@ public class FahrenheittoCelcisiusConversion {
 		double temp;
 		Scanner inputDevice= new Scanner(System.in);
 		System.out.println("what is the temp at 8?"); 
-		temp = input.nextDouble();
-
-		
+		temp = inputDevice.nextDouble();
+		displayTemp(temp, 8);
+		System.out.println("what is the temp at 12?"); 
+		temp = inputDevice.nextDouble();
+		displayTemp(temp, 12);
+		System.out.println("what is the temp at 5?"); 
+		temp = inputDevice.nextDouble();
+		displayTemp(temp, 5);
 	}
+		public static void displayTemp(double temp, int time)
+		{
+		double celcius;
+		celcius = (temp - 32)/1.8;
+		System.out.print("The temp is Celcius at " + time +" is " + celcius);
+		}
 
 }
